e7a4fd0f2544c3a56040d6fb0cd1b465abc09d06
任强 <renqiang06@126.com>
2017-03-29 20:57:18 +0800

    Revert "Delete clock.py"
    This reverts commit 8801df267aeddc256ce027906d64dde0212b162e.

---
+++ clock.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import time
+import requests
+import datetime
+import sqlite3
+from bs4 import BeautifulSoup
+import smtplib  # 导入smtplib和MIMEText
+from email.mime.text import MIMEText
+
+data = []
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit'
+                  '/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safar'
+                  'i/537.36',
+}
+
+
+def numtozh(num):
+    num_dict = {1: '一', 2: '二', 3: '三', 4: '四', 5: '五', 6: '六', 7: '七',
+                8: '八', 9: '九', 0: '零'}
+    num = int(num)
+    if 100 <= num < 1000:
+        b_num = num // 100
+        s_num = (num - b_num * 100) // 10
+        g_num = (num - b_num * 100) % 10
+        if g_num == 0 and s_num == 0:
+            num = '%s百' % (num_dict[b_num])
+        elif s_num == 0:
+            num = '%s百%s%s' % (num_dict[b_num], num_dict.get(s_num, ''), num_dict.get(g_num, ''))
+        elif g_num == 0:
+            num = '%s百%s十' % (num_dict[b_num], num_dict.get(s_num, ''))
+        else:
+            num = '%s百%s十%s' % (num_dict[b_num], num_dict.get(s_num, ''), num_dict.get(g_num, ''))
+    elif 10 <= num < 100:
+        s_num = num // 10
+        g_num = (num - s_num * 10) % 10
+        if g_num == 0:
+            g_num = ''
+        num = '%s十%s' % (num_dict[s_num], num_dict.get(g_num, ''))
+    elif 0 <= num < 10:
+        g_num = num
+        num = '%s' % (num_dict[g_num])
+    elif -10 < num < 0:
+        g_num = -num
+        num = '零下%s' % (num_dict[g_num])
+    elif -100 < num <= -10:
+        num = -num
+        s_num = num // 10
+        g_num = (num - s_num * 10) % 10
+        if g_num == 0:
+            g_num = ''
+        num = '零下%s十%s' % (num_dict[s_num], num_dict.get(g_num, ''))
+    return num
+
+
+def get_seconds(h='19', m='00', s='00'):
+    """获取当前时间与程序启动时间间隔秒数"""
+
+    # 设置程序启动的时分秒
+    time_pre = '%s:%s:%s' % (h, m, s)
+    # 获取当前时间
+    time1 = datetime.datetime.now()
+    # 获取程序今天启动的时间的字符串格式
+    time2 = time1.date().strftime('%Y-%m-%d') + ' ' + time_pre
+    # 转换为datetime格式
+    time2 = datetime.datetime.strptime(time2, '%Y-%m-%d %H:%M:%S')
+    # 判断当前时间是否晚于程序今天启动时间，若晚于则程序启动时间增加一天
+    if time1 > time2:
+        time2 = time2 + datetime.timedelta(days=1)
+
+    return time.mktime(time2.timetuple()) - time.mktime(time1.timetuple())
+
+
+def get_weather():
+    # 下载墨迹天气主页源码
+    res = requests.get('http://tianqi.moji.com/', headers=headers)
+    # 用BeautifulSoup获取所需信息
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    temp = soup.find('div', attrs={'class': 'wea_weather clearfix'}).em.getText()
+    temp = numtozh(int(temp))
+
+    weather = soup.find('div', attrs={'class': 'wea_weather clearfix'}).b.getText()
+
+    sd = soup.find('div', attrs={'class': 'wea_about clearfix'}).span.getText()
+    sd_num = re.search(r'\d+', sd).group()
+    sd_num_zh = numtozh(int(sd_num))
+    sd = sd.replace(sd_num, sd_num_zh)
+    sd = sd.replace(' ', '百分之').replace('%', '')
+
+    wind = soup.find('div', attrs={'class': 'wea_about clearfix'}).em.getText()
+
+    aqi = soup.find('div', attrs={'class': 'wea_alert clearfix'}).em.getText()
+    aqi_num = re.search(r'\d+', aqi).group()
+    aqi_num_zh = numtozh(int(aqi_num))
+    aqi = aqi.replace(aqi_num, aqi_num_zh).replace(' ', ',空气质量')
+    aqi = '空气质量指数' + aqi
+
+    info = soup.find('div', attrs={'class': 'wea_tips clearfix'}).em.getText()
+    info = info.replace('，', ',')
+
+    # 获取今天的日期
+    # today = datetime.datetime.now().date().strftime('%Y年%m月%d日')
+    today = datetime.datetime.now().strftime('%Y.%m.%d')
+    # 将获取的信息拼接成一句话
+    end = '谢谢，再见!'
+    textall = '大家好！我是任强。今天是%s,天气%s,温度%s摄氏度,%s,%s,%s,%s,%s' % \
+              (today, weather, temp, sd, wind, aqi, info, end)
+    return textall
+
+
+def lianjiaanting():
+    url = 'http://sh.lianjia.com/ershoufang/q5011000013952'
+    res = requests.get(url)
+    res = res.text.encode(res.encoding).decode('utf-8')
+    soup = BeautifulSoup(res, 'html.parser')
+    average_price = soup.find(name='span', attrs={'class': 'botline'}).strong.getText()
+    nums_houses = soup.find(name='div', attrs={'class': 'list-head clear'}).span.getText()
+
+    housenumbers = soup.find_all(name='div', attrs={'class': 'pic-panel'})
+    wheres = soup.find_all(name='div', attrs={'class': 'where'})
+    others = soup.find_all(name='div', attrs={'class': 'other'})
+    prices = soup.find_all(name='div', attrs={'class': 'col-3'})
+    viewtimes = soup.find_all(name='div', attrs={'class': 'col-2'})
+    for housenumber, where, other, price, viewtime in zip(housenumbers, wheres, others, prices, viewtimes):
+        # result = {}
+        result['housenumber'] = housenumber.find('a').get('key')
+        result['where'] = where.getText().replace('\t', '').replace('\n', ' ').replace('\xa0', '').replace('  ', ' ')
+        result['other'] = other.getText().replace('|', '').replace('\t', '').replace('\n', ' ').replace('  ', ' ')
+        result['price'] = price.getText().replace('\t', '').replace('\n', ' ').replace('  ', ' ')
+        result['viewtime'] = viewtime.getText().replace('\t', '').replace('\n', ' ').replace('  ', ' ')
+        data.append(result)
+
+    textfangjia = '下面播报今天安亭镇玉兰四村的房价情况：玉兰四村的挂牌均价为%s元，正在出售%s套二手房源。谢谢收听，明天同一时间见！' \
+                  % (average_price, nums_houses)
+    return textfangjia
+
+
+def text2voice(texts, nameofproject):
+    # 1的翻译为：http://tts.baidu.com/text2audio?idx=1&tex=222&cuid=baidu_speech_demo&cod=2&lan=zh&ctp=1&pdt=1&spd=5&per=3&vol=9&pit=5
+    # cod=2 lan=zh(语言中文) ctp=1 pdt=1 spd=4(语速1:2:9) per=4(0女2男3度逍遥4度丫丫) vol=5(0:1:9) pit=5
+    url = 'http://tts.baidu.com/text2audio?idx=1&tex={0}&cuid=baidu_speech_' \
+          'demo&cod=2&lan=zh&ctp=1&pdt=1&spd=5&per=3&vol=9&pit=5'.format(texts)
+    # 下载转换后的mp3格式语音
+    res = requests.get(url, headers=headers)
+    # 将MP3存入本地
+    with open(datetime.datetime.now().strftime('%Y.%m.%d') + '%s.mp3' % nameofproject, 'wb') as f:
+        f.write(res.content)
+
+
+def send_mail(sub, content):
+    mailto_list = ["449582502@qq.com"]  # 要发给谁，这里发给1个人
+    #####################
+    mail_host = "smtp.126.com"  # 设置服务器，用户名、口令以及邮箱的后缀
+    mail_user = "renqiang06"
+    mail_pass = "r1989q1206"
+    mail_postfix = "126.com"
+    #####################
+    me = mail_user + "<" + mail_user + "@" + mail_postfix + ">"
+    msg = MIMEText(content, _charset='gbk')
+    msg['Subject'] = sub
+    msg['From'] = me
+    msg['To'] = ";".join(mailto_list)
+    try:
+        s = smtplib.SMTP()
+        s.connect(mail_host)
+        s.login(mail_user, mail_pass)
+        s.sendmail(me, mailto_list, msg.as_string())
+        s.close()
+        return True
+    except Exception as e:
+        print(str(e))
+        return False
+
+
+def main():
+    while True:
+        deltass = 0
+        t = time.localtime()
+        hour = t.tm_hour
+        if hour > 23 or hour < 8:  # 为了晚上22点之后，上午8点之前不被打扰，设定了条件
+            print('休息时间，不提供服务')
+            time.sleep(10)
+            continue
+        else:
+            print('循环闹钟')
+            print('倒计时：')
+            while True:
+                t = time.localtime()
+                minute = t.tm_min
+                second = t.tm_sec
+                # ss = 60 * (60 - minute) + 60 - second
+                ss = 0 * 60 * (60 - minute) + 60 - second
+                if deltass == 10:  # 10秒钟更新一次倒计时
+                    print('还剩下%s秒' % ss)
+                    deltass = 0  # 重置
+                if ss > 1:
+                    time.sleep(0.5)
+                    deltass += 0.5
+                else:
+                    break
+
+        # 获取需要转换语音的文字
+        if hour == 19:
+            text = get_weather()
+            nameofproject = '天气'
+        elif hour == 22:
+            text = lianjiaanting()
+            nameofproject = '玉兰四村'
+        else:
+            text = get_weather()
+            nameofproject = '天气'
+        # 将文字转换为语音并存入程序所在文件夹
+        text2voice(text, nameofproject)
+        # 获取音乐文件绝对地址
+        mp3path2 = os.path.join(os.path.dirname(__file__), 'clockrq.mp3')
+        # 先播放一首音乐做闹钟
+        # os.system('mplayer %s' % mp3path2)
+        os.system('%s' % mp3path2)
+
+        if send_mail('python自动转发邮件', '%s' % text):
+            print('发送成功')
+        else:
+            print('发送失败')
+
+        time.sleep(30)
+        # 播报语音天气
+        mp3path1 = os.path.join(os.path.dirname(__file__),
+                                datetime.datetime.now().strftime('%Y.%m.%d') + f'{nameofproject}.mp3')
+        # os.system('mplayer %s' % mp3path1)
+        os.system('%s' % mp3path1)
+        # os.remove(mp3path1)
+        os.system('clear')
+        print(text)
+        time.sleep(30)
+
+
+if __name__ == '__main__':
+    main()
